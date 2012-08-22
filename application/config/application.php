6f19aa01ec170724ad2b714cb7b10928af3ef8d9
Marco Rivadeneyra <rockiano@gmail.com>
2012-08-22 16:03:07 -0500

    Added bootstraper clases to application.php

--- application/config/application.php
+++ application/config/application.php
@@ -155,7 +155,7 @@ return array(
 		'Event'      	=> 'Laravel\\Event',
 		'File'       	=> 'Laravel\\File',
 		'Filter'     	=> 'Laravel\\Routing\\Filter',
-		'Form'       	=> 'Laravel\\Form',
+		//'Form'       	=> 'Laravel\\Form',
 		'Hash'       	=> 'Laravel\\Hash',
 		'HTML'       	=> 'Laravel\\HTML',
 		'Input'      	=> 'Laravel\\Input',
@@ -163,7 +163,7 @@ return array(
 		'Lang'       	=> 'Laravel\\Lang',
 		'Log'        	=> 'Laravel\\Log',
 		'Memcached'  	=> 'Laravel\\Memcached',
-		'Paginator'  	=> 'Laravel\\Paginator',
+		//'Paginator'  	=> 'Laravel\\Paginator',
 		'Profiler'  	=> 'Laravel\\Profiling\\Profiler',
 		'URL'        	=> 'Laravel\\URL',
 		'Redirect'   	=> 'Laravel\\Redirect',
@@ -180,6 +180,27 @@ return array(
 		'URI'        	=> 'Laravel\\URI',
 		'Validator'  	=> 'Laravel\\Validator',
 		'View'       	=> 'Laravel\\View',
+		
+		/**
+		 * Bootstrapper
+		/**/
+		'Form'          => 'Bootstrapper\\Form',
+		'Paginator'     => 'Bootstrapper\\Paginator',
+		'Alert'         => 'Bootstrapper\\Alert',
+		'Tabbable'      => 'Bootstrapper\\Tabbable',
+		'Navigation'    => 'Bootstrapper\\Navigation',
+		'Progress'      => 'Bootstrapper\\Progress',
+		'Badges'        => 'Bootstrapper\\Badges',
+		'Labels'        => 'Bootstrapper\\Labels',
+		'DropdownButton'=> 'Bootstrapper\\DropdownButton',
+		'SplitDropdownButton'=> 'Bootstrapper\\SplitDropdownButton',
+		'ButtonGroup'   => 'Bootstrapper\\ButtonGroup',
+		'ButtonToolbar' => 'Bootstrapper\\ButtonToolbar',
+		'Navbar'        => 'Bootstrapper\\Navbar',
+		'Breadcrumbs'   => 'Bootstrapper\\Breadcrumbs',
+		'Carousel'      => 'Bootstrapper\\Carousel',
+		'Typeahead'     => 'Bootstrapper\\Typeahead',
+		'Buttons'       => 'Bootstrapper\\Buttons',
 	),
 
 );
